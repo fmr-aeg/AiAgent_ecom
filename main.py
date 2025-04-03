fd8bfd482322f2360fd136802d6dac3b448e1422
fmr-aeg <ayoub.elghaoui.pro@gmail.com>
2025-04-03 22:29:27 +0200

    evol v1

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
+import os
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, BitsAndBytesConfig
+from smolagents import CodeAgent, load_tool,tool,  Model, FinalAnswerTool, LiteLLMModel
+from custom_model import CustomTransformersModel
+from typing import Optional
+from utils_tools import get_raw_description_from_product_url, get_price_from_product_url, make_a_search_on_amazon, compare_products, ParserProductDescriptionWithoutGuideTool, ParserProductDescriptionWithGuideTool
+from Gradio_UI import GradioUI
+os.environ['HF_HOME'] = '/home/ayoub/llm_models'
+os.environ['GEMINI_API_KEY'] = 'AIzaSyDdMKL9vyRC3xEnjitbQhaRmpHrYJbgAlo'
+
+
+SYSTEM_PROMPT_GUIDED = """Tu es un assistant expert de l'extraction d'information. Tu t'appuie sur la description produit fournis par l'utilisateur pour retrouver la liste des attributs qui t'es demandé.
+Une fois les avoir extrait, tu structure ta réponse sous le format d'un json avec en clef le nom de l'attribut et en valeur sa valeur. Si jamais un des attribut demandé par l'utilisateur ne se retrouve pas dans la description, tu mets la valeur "N/A".
+Par exemple, si l'utilisateur te fournis la fiche d'un canapé et te demande de retrouver les dimensions, la couleur, la matiere et si il est convertible, tu donneras une réponse sous la forme :
+{"dimension": dimension du produit,
+ "couleur": couleur du produit,
+ "matiere": matière du produit,
+ "convertible": si oui ou non il est convertible}
+N'oublie pas que le client a besoin de toi donc donne toi à fond ! """
+
+model = LiteLLMModel(model_id='gemini/gemini-1.5-flash')
+
+product_description_parser_with_guide = ParserProductDescriptionWithGuideTool(model, SYSTEM_PROMPT_GUIDED)
+final_answer = FinalAnswerTool()
+
+agent = CodeAgent(
+    tools=[
+        # get_weather,
+        get_raw_description_from_product_url,
+        get_price_from_product_url,
+        product_description_parser_with_guide,
+        make_a_search_on_amazon,
+        compare_products,
+        final_answer
+    ],
+    model=model,
+    max_steps=3,
+    verbosity_level=2,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+    additional_authorized_imports=['pandas', 'json']
+)
+
 if __name__ == '__main__':
-    print('Hello World!')
+    GradioUI(agent).launch()
--- utils_tools.py
+++ utils_tools.py
@@ -1,18 +1,122 @@
-from smolagents import tool
+from smolagents import tool, Tool
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from PIL import Image
+from io import BytesIO
+
+
+class ParserProductDescriptionWithoutGuideTool(Tool):
+    name = "parse_product_description_without_guide"
+    description = "tool that extract every relevant information from product description provided. It determine which are the best feature to find for a given product and return a structured response using json format"
+    inputs = {"product_description": {"type": "string",
+                                      "description": "The product description containing every information on the product"},
+              }
+    output_type = "string"
+
+    def __init__(self, model,
+                 system_prompt,
+                 **kwargs):
+        super().__init__( **kwargs)
+        self.model = model
+        self.system_prompt = system_prompt
+
+    def _preprocessing_message(self, product_description):
+        messages = [{"role": "system",
+                     "content": [{"type": "text", "text": self.system_prompt}]},
+                    {"role": "user",
+                     "content": [{"type": "text", "text": product_description}]}
+                    ]
+        return messages
+
+    def forward(self, product_description: str):
+        messages = self._preprocessing_message(product_description)
+
+        return self.model(messages, response_format={"type": "json_object"}).content
+
+
+class ParserProductDescriptionWithGuideTool(Tool):
+    name = "parse_product_description_with_guide"
+    description = "tool that retrieve on a product description the value for a given list of relevant feature. It return a structured response using json format"
+    inputs = {"product_description": {"type": "string",
+                                      "description": "The product description containing every information on the product"},
+              "product_feature": {"type": "array",
+                                  "description": "The list of feature that should be retrieve from product description"},
+              }
+    output_type = "string"
+
+    def __init__(self, model,
+                 system_prompt,
+                 **kwargs):
+        super().__init__( **kwargs)
+        self.model = model
+        self.system_prompt = system_prompt
+
+    def _preprocessing_message(self, product_description, feature_list):
+
+
+        messages = [{"role": "system",
+                     "content": [{"type": "text", "text": self.system_prompt}]},
+                    {"role": "user",
+                     "content": [{"type": "text", "text": product_description},
+                                 {"type": "text", "text": f"retrieve the following features : {feature_list}"}]}
+                    ]
+        return messages
+
+    def forward(self, product_description: str, product_feature: list[str]):
+        messages = self._preprocessing_message(product_description, product_feature)
+
+        return self.model(messages, response_format={"type": "json_object"}).content
 
 
 @tool
-def get_amazon_product_description(product_url: str) -> str:
+def get_price_from_product_url(product_url: str) -> str:
     """
-    Extract a complete product description from a product on Amazon.com
+    Function that return the price of the product on Amazon
+
+    Args:
+        product_url: The url of the product sold on Amazon.com
+
+    Returns:
+        A string containing the price and the currency of the product (e.g. : '134,45$')
+    """
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9"
+    }
+
+    proxies = {
+        "https": "scraperapi.country_code=us:b3113d8cc78c7f441f071729e15055e1@proxy-server.scraperapi.com:8001"
+    }
+
+    try:
+        response = requests.get(product_url, headers=headers, proxies=proxies, verify=False)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        price = soup.find('span', class_='a-offscreen')
+
+        if price:
+            return price.get_text()
+
+        else :
+            return 'price not found'
+
+    except requests.exceptions.RequestException as e:
+        return f"Error : {str(e)}"
+
+
+@tool
+def get_raw_description_from_product_url(product_url: str) -> str:
+    """
+    Extract a raw string containing all the information available on the product on Amazon
 
     Args:
         product_url: The url of the product sell on Amazon.com
 
     Returns:
-        A string containing every information on the product
+        A string containing all the description found on the product page
     """
 
     headers = {
@@ -26,13 +130,14 @@ def get_amazon_product_description(product_url: str) -> str:
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        preprocessed_html = soup.title.string if soup.title else 'Aucun titre trouvé'
+        preprocessed_html = soup.title.string if soup.title else 'Title not found'
         preprocessed_html += "\n"
 
         # Extraction de la description
         product_description = soup.find("div", id="productDescription")
         alt_description = soup.find("div", id="feature-bullets")
         seller_description = soup.find("div", id="aplus")
+        price = soup.find('span', class_='a-offscreen')
 
         if product_description:
             preprocessed_html += "## product description by Website## \n\n"
@@ -50,3 +155,128 @@ def get_amazon_product_description(product_url: str) -> str:
 
     except requests.exceptions.RequestException as e:
         return f"Error : {str(e)}"
+
+
+@tool
+def make_a_search_on_amazon(keyword: str) -> list[dict]:
+    """
+    function to retrieve a list of products resulting from a search on the amazon search engine
+
+    Args:
+        keyword: the keyword to search for in the search engine
+
+    Returns:
+        a list containing one json per product with the following three keys :
+            - title : title of the product
+            - image_url : url of the product's image
+            - product_link : url of the product page
+    """
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+    }
+
+    url = f"https://www.amazon.com/s?k={keyword.replace(' ', '+')}"
+
+    # Envoi de la requête HTTP à Amazon
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print("Erreur lors de la récupération de la page")
+
+    # Utilisation de BeautifulSoup pour analyser le HTML de la page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Récupérer les 10 premiers produits
+    products = []
+    product_elements = soup.find_all('div', role='listitem')
+
+    for product in product_elements[:10]:  # Limiter à 10 produits
+        tag_sponsorised = product.find('span', class_='puis-label-popover-default')
+
+        if tag_sponsorised:
+            continue
+
+        title_element = product.find('h2')
+        image_element = product.find('img', class_='s-image')
+        link_element = product.find('a', class_='a-link-normal')
+
+        if title_element and image_element and link_element:
+            title = title_element.get_text()
+            image_url = image_element['src']
+            product_link = 'https://www.amazon.com' + link_element['href']
+
+            # Ajouter les données du produit dans la liste
+            products.append({
+                'title': title,
+                'image_url': image_url,
+                'product_link': product_link
+            })
+
+    return products
+
+@tool
+def compare_products(features: list[str], products: list[any]) -> pd.DataFrame:
+    """
+    Compare products based on specified features and return a dataframe to easily read the comparisons
+
+    Args:
+        features: List of features to compare (e.g., ['Price', 'Screen', 'Processor'])
+        products: List of products as dictionaries (e.g., [{'Name': 'Product 1', 'Price': 500, 'Screen': '15"', 'Processor': 'Intel i5'}, {'Name': 'Product 2', 'Price': 600, 'Screen': '17"', 'Processor': 'Intel i7'}])
+
+    Returns:
+        A pandas DataFrame representing the comparison of the products
+    """
+
+    # Créer une liste pour les lignes du tableau
+    comparison_data = []
+
+    # Ajouter les produits dans les colonnes du tableau
+    for product in products:
+        # Extraire uniquement les caractéristiques pertinentes pour ce produit
+        product_comparison = {feature: product.get(feature, 'N/A') for feature in features}
+        product_comparison['Name'] = product.get('Name', 'unknown')  # Ajouter le nom du produit comme première colonne
+        comparison_data.append(product_comparison)
+
+    # Créer un DataFrame pandas à partir de la comparaison
+    df_comparison = pd.DataFrame(comparison_data, columns=features)
+
+    return df_comparison
+
+@tool
+def compare_products_bis(products: list[any]) -> pd.DataFrame:
+    """
+    Compare products based on specified features and return a dataframe to easily read the comparisons
+
+    Args:
+        products: List of products as dictionaries (e.g., [{'Name': 'Product 1', 'Price': 500, 'Screen': '15"', 'Processor': 'Intel i5'}, {'Name': 'Product 2', 'Price': 600, 'Screen': '17"', 'Processor': 'Intel i7'}])
+
+    Returns:
+        A pandas DataFrame representing the comparison of the products
+    """
+
+    df_comparison = pd.DataFrame(products)
+
+    return df_comparison
+
+@tool
+def display_product_image(product_url: str) -> Image:
+    """
+    Download the product image from product_url and return a PIL.Image object to display the product
+
+    Args:
+        product_url: the url of the image to be downloaded
+    Returns:
+        A image in format PIL.Image downloaded from url
+    """
+    response = requests.get(product_url)
+
+    if response.status_code != 200:
+        print("Error on downloading image process")
+
+    image = Image.open(BytesIO(response.content))
+    return image
